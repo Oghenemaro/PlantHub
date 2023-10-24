503372a6e6d68ede94e042ee5f64d1008fed6311
nerdmaro@gmail.com <nerdmaro@gmail.com>
2023-10-24 23:51:42 +0100

    Added HTTP request
    Tested a GET and POST request with the hapi server

--- app.js
+++ app.js
@@ -3,13 +3,20 @@
 const Hapi = require('@hapi/hapi');
 const Hoek = require('@hapi/hoek');
 const Vision = require('@hapi/vision');
+const Path = require('path');
+// destructuring or not
+const { MongoClient } = require('mongodb');
 
-const server = Hapi.server({
-    port: 2000,
-    host: 'localhost'
-});
+const url = 'mongodb://admin:password@localhost:27017';
+const client = new MongoClient(url);
+const dbname = 'users';
 
-const init = async () => {
+const planthub = async () => {
+    const server = Hapi.server({
+        host: '127.0.0.1',
+        port: 3000
+    });
+
     await server.register(Vision);
 
     server.views({
@@ -23,22 +30,46 @@ const init = async () => {
     server.route({
         method: 'GET',
         path: '/',
-        handler: (request, h) => {
-            return h.view('index', {
-                title: 'Handlebar working'
-            });
+        handler: (req, h) => {
+            return 'We up';
+
+        }
+
+    });
+    
+    server.route({
+        method: 'POST',
+        path: '/',
+        handler: async (req, h) => {
+
+            let values = req.payload;
+            values['userid'] = 1;
+            let query = {userid: 1};
+            let updatedValues = {$set: values};
+            let result;
+            try {
+                await client.connect();
+                console.log('Connection successful');
+                const db = client.db(dbname);
+                const dbcollection = db.collection('user');
+                result = await dbcollection.insertOne(values);
+                // result = await dbcollection.updateOne(query, updatedValues, {upset: true});
+                console.log('update successfull');
+            } catch (err) {
+                console.log(err);
+            } finally {
+                client.close();
+            }
+            return result;
         }
     });
-
     
-
     await server.start();
-    console.log('Server running on %s', server.info.uri);
-};
+    console.log('Server Operational', server.info.uri);
+    process.on('unhandledRejection', (err) => {
+        console.log(err);
+        process.exit(1);
+    });
+}
 
-process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
-});
-
-init();
+planthub();
